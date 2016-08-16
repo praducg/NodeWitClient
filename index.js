676c8689a75baab2fb1ffe5b04d30e2c06786de6
Pradeep <praducg@gmail.com>
2016-08-17 00:53:59 +0530

    Add files via upload
    17th uploaded files

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-'use strict';
 module.exports = {
   log: require('./lib/log'),
   Wit: require('./lib/wit'),
+
+  interactive: require('./lib/interactive')
 }
-let log = null;
 const bodyParser = require('body-parser');
-log = require('./lib/log.js').log;
+//const Wit = require('./').Wit
+var AWS = require('aws-sdk');
+var lambda = new AWS.Lambda();
+var msg = '';
 // Webserver parameter
-const PORT = process.env.PORT || 8445;
+const PORT = process.env.PORT || 8005;
+
+const crypto = require('crypto');
+const fetch = require('node-fetch');
+const request = require('request');
 
 var express = require('express')
   , cors = require('cors')
@@ -15,13 +22,22 @@ var express = require('express')
 //fix for security format exception from browser.
 app.use(cors());
 
+let log = null;
+let Wit = null;
+try {
+  Wit = require('./').Wit;// if running from repo
+  log = require('./').log;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  log = require('node-wit').log;
+}
 // Starting our webserver and putting it all together
 var path = require("path");
 app.set('port', PORT);
 app.listen(app.get('port'));
 app.use(bodyParser.json());
 
-console.log("Server is running at  @" + PORT);
+console.log("Server is wating for you @" + PORT);
 
 const firstEntityValue = (entities, entity) => {
   const val = entities && entities[entity] &&
@@ -44,104 +60,69 @@ const getFirstMessagingEntry = (body) => {
     Array.isArray(body.entry[0].messaging) &&
     body.entry[0].messaging.length > 0 &&
     body.entry[0].messaging[0];
-
   return val || null;
 };
 
 const actions = {
-  say(sessionId, context, message, cb) {
-    console.log(message);
-    cb();
-  },
-  merge(sessionId, context, entities, message, cb) {
-    // Retrieve the location entity and store it into a context field
-    const loc = firstEntityValue(entities, 'location');
-    if (loc) {
-      context.loc = loc;
-	  console.log("location::" + loc);
-    }
-    cb(context);
-  },
-  error(sessionId, context, error) {
-    console.log(error.message);
+  send(request, response) {
+	  msg='';
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+	return new Promise(function(resolve, reject) {
+      console.log('sending...', JSON.stringify(response));
+	  let resl = resolve();
+	  msg = msg + JSON.stringify(text) + '\n' + JSON.stringify(entities);
+      return resl;
+    });
   },
 };
 
 const sessions = {};
-
-const findOrCreateSession = () => {
-  var sessionId;
+const findOrCreateSession = (sid) => {
+  let sessionId;
+  // Let's see if we already have a session for the user
+  Object.keys(sessions).forEach(k => {
+    if (sessions[k].sid === sid) {
+      // Yep, got it!
+      sessionId = k;
+    }
+  });
     // No session found, let's create a new one
+	if (!sessionId) {
     sessionId = new Date().toISOString();
-    sessions[sessionId] = {
-      sessionId: sessionId,
-      context: {
-        _sessionId_: sessionId
-      }
-    }; // set context, _sessionId_
+    sessions[sessionId] = { sessionId: sessionId,  context: { sid: sessionId } }; // set context, sessionId
+	}
   return sessionId;
 };
 
-app.get('/botTest.html', function(request, response, next){
-    response.sendFile(path.join(__dirname+'/botTest.html'));
+app.get('/lambdaServiceCall.html', function(request, response, next){
+    response.sendFile(path.join(__dirname+'/lambdaServiceCall.html'));
 });
+function handleResponseFromLamda(err,response)
+{
+	if(err)
+	{
+		console.log("Problem calling the lambda service");
+		return;
+	}
+	console.dir(response);
+}
+function runFunctionOnLambda(userKey){
+	lambda.invoke(userKey,handleResponseFromLamda);
+}
 
-// callwit is the main service which takes care of calling wit.ai
-app.post('/callwit', (req, res) => {
-	// Parsing the Messenger API response
-	const messaging = getFirstMessagingEntry(req.body);
-	const wit = new Wit({
-	  accessToken: 'W47X2VGODXS4P22XQHCPHGD2X7RDQTY7',
-	  actions,
-	  logger: new log.Logger(log.INFO)
-	});
-	
-	
-	//const wit = new Wit('W47X2VGODXS4P22XQHCPHGD2X7RDQTY7', actions);
-	console.log('wit client object: ' + wit);
-	
-    // We retrieve the user's current session, or create one if it doesn't exist
-    // This is needed for our bot to figure out the conversation history
-    const sessionId = findOrCreateSession();
-	console.log('sessionID: ' + sessionId);
-	// We retrieve the message content
-    const msg = messaging.message.text;
-	console.log('user message: ' + msg);
-	if (msg) {
-      // We received a text message
-      // Let's forward the message to the Wit.ai Bot Engine, This will run all actions until our bot has nothing left to do
-      wit.runActions(
-        sessionId, // the user's current session
-        msg, // the user's message 
-        sessions[sessionId].context, // the user's current session state
-        (error, context) => {
-          if (error) {
-            console.log('Oops! Got an error from Wit:', error);
-          } else {
-            console.log('Waiting for futher messages.');
-            // Updating the user's current session state
-            sessions[sessionId].context = context;
-          }
-		  console.log('final msg: '+wit.respMsg());
-		  res.send(wit.respMsg())
-        }
-      );
-    }
-	console.log('Response Object: ' + res);
-	
-  //res.send('"Only those who will risk going too far can possibly find out how far one can go." - Chandra');
- // next();
+// Message handler
+
+app.post('/DEV/bankingbot/accountsummary', (req, res) => {
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // Parse the Messenger payload
+  // See the Webhook reference
+  // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+  const data = req.body;
+  //const wit = new Wit({accessToken: 'W47X2VGODXS4P22XQHCPHGD2X7RDQTY7', actions, logger: new log.Logger(log.INFO)});
+AWS.config.update({accessKeyId: '', secretAccessKey: ''});
+runFunctionOnLambda({userKey:data.userKey});
+
+
+    
 });
-
-// another example post service
-app.post('/webhook', (req, res, next) => {
-  res.send('"Only those who will risk going too far can possibly find out how far one can go." - Chandra');
-  next();
-});
-
-// Webhook get method sends status 400
-app.get('/webhook', (req, res, next) => {
-    res.sendStatus(400);
-	next();
-});
-
