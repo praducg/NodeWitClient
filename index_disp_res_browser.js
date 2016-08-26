a7d196390939edaca1c82301a6a7a6359de7732f
Pradeep <praducg@gmail.com>
2016-08-26 21:52:22 +0530

    extra condition to check accountype
    Added an extra condition to check the account type in lambda service

--- index_disp_res_browser.js
+++ index_disp_res_browser.js
@@ -15,32 +15,56 @@ app.listen(app.get('port'));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 console.log("Server is running at " + PORT);
-var body='';
+var res = '';
 //Function to call lambda service as of now input is hardcoded
-function callLambdaService(userKey, callback) {
-    var options = {
-        uri : 'https://hu4xwdeme9.execute-api.us-east-1.amazonaws.com/DEV/bankingbot/accountsummary?userKey='+userKey,
-        method : 'GET'
-    }; 
-    var res = '';
-    request(options, function (error, response, body) {
+function callLambdaService(userKey,accounttype,callback) {
+	//To check whether accountType is empty or null or undefined
+	if(!accounttype || /^\s*$/.test(accounttype)){
+		var options = {
+			uri : 'https://hu4xwdeme9.execute-api.us-east-1.amazonaws.com/DEV/bankingbot/accountsummary?userKey='+userKey,
+			method : 'GET'
+		}; 
+		request(options, function (error, response, body) {
+			
+			console.log(response);
+			if (!error && response.statusCode == 200) {
+				res = body;
+			}
+			else {
+				res = 'Not Found';
+			}
+			callback(res);
+		});
+	}
+	else
+	{
+		var options = {
+			uri : 'https://hu4xwdeme9.execute-api.us-east-1.amazonaws.com/DEV/bankingbot/accountsummary?userKey='+userKey+'&accountType='+accounttype,
+			method : 'GET'
+		}; 
+		request(options, function (error, response, body) {
+			
+			console.log(response);
+			if (!error && response.statusCode == 200) {
+				res = body;
+			}
+			else {
+				res = 'Not Found';
+			}
+			callback(res);
+		});
 		
-		console.log(response);
-        if (!error && response.statusCode == 200) {
-            res = body;
-        }
-        else {
-            res = 'Not Found';
-        }
-        callback(res);
-    });
+		
+		
+	}
 }
 //Call a method which hits the aws url and get the response back 
-callLambdaService("1001", function(resp){
+callLambdaService("1001","CHECKING", function(resp){
 	//Hurray Got response ..Now convert this to string
 	body=JSON.stringify(resp);;
 });
-app.get('/',function(req,res){
+//To check the response in browser 
+//app.get('/',function(req,res){
 	//Send this response to browser
-    res.send(body);
-});
+//	res.send(body);
+//});
